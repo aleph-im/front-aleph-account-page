2bea909e0a44b6f5eec6c91a1e9577e077558199
amalcaraz <amalcaraz89@gmail.com>
2023-11-03 02:24:34 +0100

    desktop sidebar animation

--- src/components/common/Sidebar/cmp.tsx
+++ src/components/common/Sidebar/cmp.tsx
@@ -1,7 +1,6 @@
 import { IconName } from '@fortawesome/fontawesome-svg-core'
 import { Icon } from '@aleph-front/aleph-core'
 import {
-  StyledLinkContent,
   StyledLogo,
   StyledNav1,
   StyledNav1Link,
@@ -11,6 +10,7 @@ import {
   StyledNav2Title,
   StyledProgressBar,
   StyledSidebar,
+  StyledStorageContainer,
   StyledToggleButton,
   StyledToggleButtonContainer,
 } from './styles'
@@ -21,6 +21,7 @@ import {
   useCallback,
   useMemo,
   useState,
+  MouseEvent,
 } from 'react'
 import { useRouter } from 'next/router'
 import { useUserStoreAllowance } from '@/hooks/common/useUserStoreAllowance'
@@ -72,30 +73,27 @@ const routes: Route[] = [
 export type SidebarLinkProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
   href: string
   icon?: IconName
+  isOpen?: boolean
   children?: ReactNode
-  open: boolean
 }
 
 export const SidebarLink = memo(
-  ({ href, icon, children, open }: SidebarLinkProps) => {
+  ({ href, icon, children, isOpen }: SidebarLinkProps) => {
     const router = useRouter()
     const isActive = router.pathname.indexOf(href) >= 0
 
-    const props = { href, $isActive: isActive, $open: open }
-    const iconProps = { name: icon as IconName, size: 'lg', tw: 'p-1' }
+    const props = { href, $isActive: isActive, $isOpen: isOpen }
+
+    const iconCmp = icon && (
+      <Icon tw="p-1" {...{ name: icon as IconName, size: 'lg' }} />
+    )
 
     return !children ? (
-      <StyledNav1Link {...props}>
-        <StyledLinkContent {...props}>
-          <span>{icon && <Icon {...iconProps} />}</span>
-        </StyledLinkContent>
-      </StyledNav1Link>
+      <StyledNav1Link {...props}>{iconCmp}</StyledNav1Link>
     ) : (
       <StyledNav2Link {...props}>
-        <StyledLinkContent {...props}>
-          {icon && <Icon {...iconProps} />}
-          <span>{children}</span>
-        </StyledLinkContent>
+        {iconCmp}
+        <span>{children}</span>
       </StyledNav2Link>
     )
   },
@@ -103,11 +101,15 @@ export const SidebarLink = memo(
 SidebarLink.displayName = 'SidebarLink'
 
 export const Sidebar = memo(() => {
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState<boolean | undefined>(undefined)
 
   const handleToggle = useCallback(() => {
-    setOpen((open) => !open)
+    setOpen((open) => (open === undefined ? false : !open))
   }, [setOpen])
+
+  const handlePreventToggle = useCallback((e: MouseEvent) => {
+    e.stopPropagation()
+  }, [])
 
   const router = useRouter()
 
@@ -122,45 +124,34 @@ export const Sidebar = memo(() => {
   const storePercent = allowedSize ? consumedSize / allowedSize : 0
 
   return (
-    <StyledSidebar $open={open}>
-      <StyledNav1 $open={open}>
+    <StyledSidebar $isOpen={open}>
+      <StyledNav1>
         <StyledLogo />
-
         {routes.map((child) => (
           <SidebarLink
             key={child.path}
-            href={child.path}
-            icon={child.icon}
-            open={open}
+            {...{
+              href: child.path,
+              icon: child.icon,
+              isOpen: open,
+            }}
           />
         ))}
-
-        <SidebarLink
-          href="/earn/bookmark"
-          icon="bookmark"
-          open={open}
-        ></SidebarLink>
-        <SidebarLink
-          href="/earn/dropbox"
-          icon="dropbox"
-          open={open}
-        ></SidebarLink>
+        <SidebarLink href="/earn/bookmark" icon="bookmark"></SidebarLink>
+        <SidebarLink href="/earn/dropbox" icon="dropbox"></SidebarLink>
       </StyledNav1>
-      <StyledNav2 $open={open}>
-        <StyledNav2LinkContainer>
+      <StyledNav2 onClick={handleToggle}>
+        <StyledNav2LinkContainer onClick={handlePreventToggle}>
           {currentRoute?.children && (
             <>
               {currentRoute?.name && (
-                <StyledNav2Title>
-                  <span>{currentRoute?.name}</span>
-                </StyledNav2Title>
+                <StyledNav2Title>{currentRoute?.name}</StyledNav2Title>
               )}
               {currentRoute?.children.map((child) => (
                 <SidebarLink
                   key={child.path}
                   href={child.path}
                   icon={child?.icon || currentRoute?.icon}
-                  open={open}
                 >
                   {child.name}
                 </SidebarLink>
@@ -168,20 +159,20 @@ export const Sidebar = memo(() => {
             </>
           )}
         </StyledNav2LinkContainer>
-        <div tw="flex-1" onClick={handleToggle} />
+        <div tw="flex-1" />
         <div tw="py-12">
           <StyledToggleButtonContainer>
-            <StyledToggleButton $open={open} onClick={handleToggle} />
+            <StyledToggleButton />
           </StyledToggleButtonContainer>
-          <div tw="max-w-[168px] mx-auto px-1">
-            <div className="tp-body3 fs-12" tw="mb-4 flex gap-1 flex-wrap">
+          <StyledStorageContainer onClick={handlePreventToggle}>
+            <div tw="mb-4 flex gap-1 flex-wrap">
               {consumedSize.toFixed(3)} GB
               <span tw="opacity-60 font-normal">
                 of {allowedSize.toFixed(3)}
               </span>
             </div>
             <StyledProgressBar $percent={storePercent} />
-          </div>
+          </StyledStorageContainer>
         </div>
       </StyledNav2>
     </StyledSidebar>
--- src/components/common/Sidebar/styles.tsx
+++ src/components/common/Sidebar/styles.tsx
@@ -1,117 +1,50 @@
 import { Icon, Logo, addClasses } from '@aleph-front/aleph-core'
 import Link from 'next/link'
-import styled, { css } from 'styled-components'
+import styled, { css, keyframes } from 'styled-components'
 import tw from 'twin.macro'
 
-export const StyledSidebar = styled.aside<{ $open?: boolean }>`
-  ${tw`flex items-stretch justify-start`}
-  transition: all linear 0.25s 0s;
-
-  ${({ $open }) =>
-    $open
-      ? css`
-          width: 23.25rem;
-        `
-      : css`
-          width: 4.875rem;
-        `};
+export const StyledLinkContentCss = css`
+  ${tw`flex items-center justify-start gap-2.5 py-2 px-6 whitespace-nowrap w-full`}
 `
 
-// -------------- NAV 1 -----------------
-
-export const StyledLogo = styled(Logo).attrs((props) => {
-  return {
-    ...props,
-    size: '1.5rem',
-    text: '',
-  }
-})`
-  ${tw`relative h-8 mt-8 mb-12`}
-`
-
-export const fadeInOutContentCss = css<{ $open?: boolean }>`
-  ${({ $open }) => css`
-    opacity: ${$open ? '1' : '0'};
-    visibility: ${$open ? 'visible' : 'hidden'};
-    transition: all linear 0.25s 0s;
-  `}
-`
-export const StyledNav1 = styled.nav<{ $open?: boolean }>`
-  ${tw`relative h-full flex flex-col items-center overflow-hidden`}
-
+export const StyledNav1 = styled.nav`
+  ${tw`h-full w-full`}
   background-color: #0000004c;
-  transition: all linear 0.25s 0s;
-
-  ${({ $open }) =>
-    $open
-      ? css`
-          width: 4.5rem;
-        `
-      : css`
-          width: 0.375rem;
-        `};
-
-  & ${StyledLogo} {
-    ${fadeInOutContentCss}
-  }
 `
 
 export const StyledNav1Link = styled(Link).attrs(addClasses('tp-nav'))<{
   $isActive?: boolean
-  $open?: boolean
 }>`
-  ${({ theme, $isActive, $open }) => css`
-    ${tw`relative w-full`}
+  ${({ theme, $isActive }) => css`
+    ${StyledLinkContentCss}
+    ${tw`relative justify-center`}
 
-    color: ${theme.color.main0};
-    opacity: ${!$isActive ? '0.4' : '1'};
-    background-color: ${$isActive && !$open
-      ? theme.color.main0
-      : 'transparent'};
-    transition: all linear 0.25s 0s;
+    color: ${theme.color.main0}${$isActive ? 'ff' : '66'};
+
+    ${$isActive &&
+    css`
+      &::after {
+        ${tw`absolute -top-2 left-0 h-full opacity-0`}
+        content: '';
+        width: 0.375rem;
+        background-color: ${theme.color.main0};
+      }
+    `}
   `}
 `
 
-// -------------- NAV 2 -----------------
-
-export const StyledNav2 = styled.nav<{ $open?: boolean }>`
-  ${tw`flex flex-col h-full overflow-hidden`}
-
-  background-color: #00000020;
+export const StyledNav2 = styled.nav`
+  ${tw`flex flex-col h-full overflow-hidden z-10`}
   padding-top: 7rem;
   background-color: #00000020;
-  transition: all linear 0.25s 0s;
-
-  ${({ $open }) =>
-    $open
-      ? css`
-          width: 18.75rem;
-          /* backdrop-filter: blur(0px); */
-
-          ${StyledNav2Title} {
-            font-size: 1.125rem;
-          }
-        `
-      : css`
-          width: 4.5rem;
-          /* backdrop-filter: blur(999px); */
-          background-color: #100f20;
-
-          ${StyledNav2Title} {
-            ${tw`px-0 w-full`}
-            font-size: 0.75rem;
-          }
-        `};
 `
 
 export const StyledNav2Title = styled.div.attrs(addClasses('tp-nav'))`
-  ${tw`relative py-2 px-6 w-0 text-center`}
-  transition: all linear 0.25s 0s;
+  ${tw`py-2 px-6 w-auto`}
 `
 
-export const StyledNav2LinkContainer = styled.div<{ $open?: boolean }>`
-  ${tw`relative flex flex-col items-start`}
-  transition: all linear 0.25s 0s;
+export const StyledNav2LinkContainer = styled.div`
+  ${tw`flex flex-col items-start cursor-auto`}
 `
 
 export const StyledNav2Link = styled(Link).attrs(addClasses('tp-nav'))<{
@@ -119,56 +52,44 @@ export const StyledNav2Link = styled(Link).attrs(addClasses('tp-nav'))<{
   $open?: boolean
 }>`
   ${({ theme, $isActive }) => css`
-    ${tw`relative w-full`}
+    ${StyledLinkContentCss}
     color: ${$isActive ? theme.color.main0 : theme.color.base0};
-  `}
-`
-
-export const StyledLinkContent = styled.div<{ $open?: boolean }>`
-  ${({ $open }) => css`
-    ${tw`relative flex items-center justify-start gap-2.5 py-2 px-6 whitespace-nowrap w-full`}
-    transition: all linear 0.25s 0s;
-
-    ${!$open &&
-    css`
-      padding-left: 1.75rem;
-    `}
 
     & > span {
-      ${fadeInOutContentCss}
+      ${tw`overflow-hidden`}
     }
   `}
 `
 
-// ----------------------------------------
+export const StyledLogo = styled(Logo).attrs((props) => {
+  return {
+    ...props,
+    size: '1.5rem',
+    text: '',
+  }
+})(() => [tw`flex items-center justify-center h-8 mt-8 mb-12`])
 
 export const StyledToggleButtonContainer = styled.div`
   ${tw`mb-16 px-6 text-left`}
-  perspective-origin: 0% 50%;
-  perspective: 2rem;
 `
 
-export type StyledToggleButtonProps = {
-  $open?: boolean
-}
-
-export const StyledToggleButton = styled(Icon).attrs(
-  (props: StyledToggleButtonProps) => {
-    return {
-      ...props,
-      name: 'angle-right', //props.$open ? 'angle-left' : 'angle-right',
-    }
-  },
-)<StyledToggleButtonProps>`
-  ${({ theme, $open }) => css`
-    ${tw`relative !w-4 !h-4 p-0.5 !box-border cursor-pointer`}
-    ${!$open && tw`!ml-1`}
-
+export const StyledToggleButton = styled(Icon).attrs((props) => {
+  return {
+    ...props,
+    name: 'angle-right', //props.$open ? 'angle-left' : 'angle-right',
+  }
+})`
+  ${({ theme }) => css`
+    ${tw`!w-4 !h-4 p-0.5 !box-border cursor-pointer`}
     color: ${theme.color.base1};
     background-color: ${theme.color.base0};
-    transition: transform linear 0.25s 0s;
-    transform: ${$open ? 'rotateY(-180deg)' : 'rotateY(0deg)'};
   `}
+`
+
+export const StyledStorageContainer = styled.div.attrs(
+  addClasses('tp-body3 fs-12'),
+)`
+  ${tw`max-w-[168px] mx-auto px-1 cursor-auto`}
 `
 
 // https://github.com/aleph-im/aleph-account/blob/8b920e34fab9f4f70e3387eed2bd5839ae923971/src/layouts/MainLayout.vue#L131C14-L131C14
@@ -198,3 +119,189 @@ export const StyledProgressBar = styled.div<{ $percent: number }>(
     ]
   },
 )
+
+const fadeOutIn1 = keyframes` 
+  0%, 15%, 90%, 100%  {
+    opacity: 1;
+  }
+
+  45% {
+    opacity: 0;
+  }
+`
+
+// @note: https://stackoverflow.com/a/43575547
+const fadeOutIn1Reverse = keyframes` 
+  0%, 10%, 80%, 100% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0;
+  }
+`
+
+const fadeOutIn2 = keyframes` 
+  0%, 6%, 80%, 100%  {
+    opacity: 1;
+  }
+
+  46%, 60% {
+    opacity: 0;
+  }
+`
+
+const fadeOutIn2Reverse = keyframes` 
+  0%, 10%, 70%, 100% {
+    opacity: 1;
+  }
+
+  30% {
+    opacity: 0;
+  }
+`
+
+export const StyledSidebar = styled.aside<{
+  $isOpen?: boolean
+  $speed?: number
+}>`
+  ${tw`flex items-stretch justify-start`}
+
+  ${({ $isOpen, $speed = 1 }) =>
+    $isOpen || $isOpen === undefined
+      ? css`
+          & ${StyledNav1} {
+            width: 4.5rem;
+            transition: width ease-in-out ${0.35 / $speed}s ${0.4 / $speed}s;
+          }
+
+          & ${StyledNav1Link} > svg,
+          & ${StyledLogo} {
+            opacity: 1;
+            visibility: visible;
+
+            transition: opacity ease-in-out ${0.2 / $speed}s ${0.45 / $speed}s,
+              visibility linear ${0.2 / $speed}s ${0.45 / $speed}s;
+          }
+
+          & ${StyledNav1Link}::after {
+            ${tw`-top-2 opacity-0`}
+            transition: opacity ease-in-out ${0.7 / $speed}s ${0.2 / $speed}s,
+              top ease-in-out ${0.7 / $speed}s ${0.1 / $speed}s;
+          }
+
+          & ${StyledNav2} {
+            width: 18.75rem;
+            transition: width ease-in-out ${0.5 / $speed}s ${0.1 / $speed}s;
+
+            &:hover {
+              cursor: w-resize;
+            }
+          }
+
+          & ${StyledNav2LinkContainer} {
+            width: 100%;
+            transition: width linear 0s ${0.5 / $speed}s;
+            animation: ${1 / $speed}s ease-in-out 0s ${fadeOutIn1Reverse};
+          }
+
+          & ${StyledNav2Title}, & ${StyledNav2Link} > svg {
+            ${tw`relative left-0 translate-x-0`}
+            transition: all linear 0s ${0.5 / $speed}s;
+          }
+
+          & ${StyledNav2Title} {
+            font-size: 1.125rem;
+          }
+
+          & ${StyledNav2Link} > span {
+            max-width: 100%;
+            transition: max-width linear 0s ${0.5 / $speed}s;
+          }
+
+          & ${StyledToggleButton} {
+            transform: rotateZ(-180deg);
+            transition: transform ease-in-out ${0.6 / $speed}s ${0.4 / $speed}s;
+          }
+
+          & ${StyledStorageContainer} {
+            animation: ${1 / $speed}s ease-in-out 0s ${fadeOutIn2Reverse};
+          }
+        `
+      : css`
+          & ${StyledNav1} {
+            width: 0.375rem;
+            transition: width ease-in-out ${0.2 / $speed}s ${0.15 / $speed}s;
+          }
+
+          & ${StyledNav1Link} > svg,
+          & ${StyledLogo} {
+            opacity: 0;
+            visibility: hidden;
+
+            transition: opacity ease-in-out ${0.2 / $speed}s 0s,
+              visibility linear ${0.2 / $speed}s 0s;
+          }
+
+          & ${StyledNav1Link}::after {
+            ${tw`top-0 opacity-100`}
+            transition: opacity ease-in-out ${0.7 / $speed}s ${0.2 / $speed}s,
+              top ease-in-out ${0.7 / $speed}s ${0.3 / $speed}s;
+          }
+
+          & ${StyledNav2} {
+            width: 4.5rem;
+
+            padding-left: 0;
+            box-shadow: 0px 0px 0px 0px #00000020;
+
+            transition: width ease-in-out ${0.4 / $speed}s ${0.25 / $speed}s,
+              padding-left ease-in-out ${0.4 / $speed}s 0s,
+              box-shadow ease-in-out ${0.4 / $speed}s 0s;
+
+            &:hover {
+              cursor: e-resize;
+              padding-left: 0.375rem;
+              box-shadow: 0.375rem 0px 0px 0px #00000020;
+            }
+          }
+
+          & ${StyledNav2LinkContainer} {
+            width: 4.5rem;
+            transition: width linear 0s ${0.45 / $speed}s;
+            animation: ${1 / $speed}s ease-in-out 0s ${fadeOutIn1};
+          }
+
+          & ${StyledNav2Title}, & ${StyledNav2Link} > svg {
+            ${tw`relative left-1/2 -translate-x-1/2`}
+            transition: all linear 0s ${0.45 / $speed}s;
+          }
+
+          & ${StyledNav2Title} {
+            font-size: 0.75rem;
+          }
+
+          & ${StyledNav2Link} > span {
+            max-width: 0;
+            transition: max-width linear 0s ${0.45 / $speed}s;
+          }
+
+          & ${StyledToggleButton} {
+            transform: rotateZ(0deg);
+            transition: transform ease-in-out ${0.6 / $speed}s ${0.25 / $speed}s;
+          }
+
+          & ${StyledStorageContainer} {
+            animation: ${1 / $speed}s ease-in-out 0s ${fadeOutIn2};
+          }
+        `};
+
+  ${({ $isOpen }) =>
+    $isOpen === undefined &&
+    css`
+      & * {
+        animation: none !important;
+        transition: none !important;
+      }
+    `}
+`
