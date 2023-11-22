5c1bc9f7a753d23fd2dba03c49f7aee351840f04
amalcaraz <amalcaraz89@gmail.com>
2023-11-22 09:56:01 +0100

    fix: remove mocked wallets

--- src/store/account.ts
+++ src/store/account.ts
@@ -47,17 +47,16 @@ export function getAccountReducer(): AccountReducer {
 
         // @note: avoid performance issues
         // @todo: Fix it on the wallet picker component / header which is firing multiple connect events
-        if (state.account?.address === account.address)
-          return state
+        if (state.account?.address === account.address) return state
 
-          // Mock CCN owner
-        ;(account as any).address = '0xc4CBB0672b2FEbE0D3eC2fD48Be3809078e6593b'
+        // Mock CCN owner
+        // ;(account as any).address = '0xc4CBB0672b2FEbE0D3eC2fD48Be3809078e6593b'
 
         // Mock CRN owner
         // ;(account as any).address = '0xD20212929FDe829C57f93dbfF9332f3FC8703BCD'
 
         // Not fully linked node
-        ;(account as any).address = '0x12AcF2e0FBB31972ccE4a97F90A013Ba58585e5c'
+        // ;(account as any).address = '0x12AcF2e0FBB31972ccE4a97F90A013Ba58585e5c'
 
         return {
           ...state,
