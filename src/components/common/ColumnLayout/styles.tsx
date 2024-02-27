6685fcacf1b50950ef334e21e676986f96ccc5c8
amalcaraz <amalcaraz89@gmail.com>
2024-02-27 08:58:19 +0100

    fix: column layout fixes in safari

--- src/components/common/ColumnLayout/styles.tsx
+++ src/components/common/ColumnLayout/styles.tsx
@@ -2,9 +2,9 @@ import styled from 'styled-components'
 import tw from 'twin.macro'
 
 export const StyledLayout = styled.div`
-  ${tw`columns-xs gap-9`}
+  ${tw`columns-xs gap-9 -mt-9`}
 
   & > * {
-    ${tw`flex flex-col gap-9 mb-9 break-inside-avoid`}
+    ${tw`inline-flex w-full flex-col gap-9 mt-9 break-inside-avoid`}
   }
 `
